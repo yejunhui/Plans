01d4776d2d025627a18a66237568680e24d8829e
yejunhui <1589082159@qq.com>
2019-10-06 12:44:19 +0800

    第二次

---
+++ ExcelData.py
@@ -0,0 +1,7 @@
+#定义模板数据
+templateData = [['模板', None, None, None, None, None, None, None, None, None, None],
+        ['请按字段填写数据', None, None, None, None, None, None, None, None, None, None],
+        ['不可更改表名《Template》', None, None, None, None, None, None, None, None, None, None],
+        ['层次', '代用', '行号', '品号', '品名', '规格', '附加属性', '商家/品牌', '单位', 'BOM数量','Bom描述']]
+#定义计划数据
+planData = []
